1d78fa1823f0548a15b861b28e4b36dfc7cb9851
strnadaljaz <strnadaljaz23@gmail.com>
2026-01-16 16:39:21 +0100

    added favicon and changed navbar to the more basic design

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -3,7 +3,15 @@
 export default function Hero() {
     return (
         <div id="Domov" className="bg-[#4CAF50] h-[calc(100vh-6rem)] flex items-center justify-center">
-            <p className="text-[100px]">Serbus</p>  
+            <div>
+                70l
+            </div>
+            <div>
+                45l
+            </div>
+            <div>
+                Jumbo
+            </div>
         </div>
     );
 }
--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -6,16 +6,13 @@ const spanStyle = "bg-left-bottom bg-gradient-to-r from-[#6B4226] to-[#6B4226]  
 export default function Navbar() {
     return (
         <nav className="relative top-0 w-full bg-[#4CAF50] h-[6rem] p-4 z-50">
-            <div className='flex items-center justify-between h-full max-w-7xl mx-auto'>
-                <h1 className='text-2xl md:text-3xl lg:text-4xl font-bold text-white tracking-wide text-[#F5F5DC]'>
-                    Kokovit
-                </h1>
-                <ul className="flex items-center justify-end h-full space-x-4 md:space-x-8 lg:space-x-16 xl:space-x-20">
-                    <li><a className={aStyle} href="#Domov"><span className={spanStyle}>Domov</span></a></li>
-                    <li><a className={aStyle} href="#Produkti"><span className={spanStyle}>Produkti</span></a></li>
-                    <li><a className={aStyle} href="#O nas"><span className={spanStyle}>O nas</span></a></li>
-                    <li><a className={aStyle} href="#Povprasevanje"><span className={spanStyle}>Povprasevanje</span></a></li>
-                    <li><a className={aStyle} href="#Kontakt"><span className={spanStyle}>Kontakt</span></a></li>
+            <div className='flex items-center justify-center h-full max-w-7xl mx-auto'>
+               <ul className="flex items-center justify-center h-full space-x-4 md:space-x-8 lg:space-x-16 xl:space-x-20">
+                    <li><a className={aStyle} href="#Domov">Domov</a></li>
+                    <li><a className={aStyle} href="#Produkti">Produkti</a></li>
+                    <li><a className={aStyle} href="#O nas">O nas</a></li>
+                    <li><a className={aStyle} href="#Povprasevanje">Povprasevanje</a></li>
+                    <li><a className={aStyle} href="#Kontakt">Kontakt</a></li>
                 </ul>
             </div>
         </nav>
