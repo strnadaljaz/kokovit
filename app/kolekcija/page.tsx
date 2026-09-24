248d4e817d7a2f48147c64b94bf729e3b5da017d
Aljaž Strnad <strnadaljaz23@gmail.com>
2026-09-24 18:39:52 +0200

    popravljen desing na strani kolekcija. Grid gre hitreje v 2 stolpca

--- app/kolekcija/page.tsx
+++ app/kolekcija/page.tsx
@@ -47,7 +47,7 @@ const Merch = () => {
                         </p>
                     </div>
 
-                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                         {products &&
                             products.map((product, i) => (
                                 <div
