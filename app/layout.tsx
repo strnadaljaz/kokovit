c9f953f04b2764203d1ffb1261082f6b7facd8e6
Aljaž Strnad <strnadaljaz23@gmail.com>
2026-09-25 14:29:53 +0200

    dodan keyword

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,35 +5,35 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Kokovit",
-  description: "Visokokvaliteten naravni substrat iz mešanice kokosovih vlaken, šote in drugih organskih snovi, za visoke grede, vrtove, rastlinjake, okrasne grede, ločnice itd.",
-  keywords: "Kokovit, naravni substrat, kokovit, kokosova vlakna, kokos, vlakna, visoke grede, grede, vrt, rastlinjaki, rastlinjak, vrtičkanje, substrat za vrt, organski substrat, substrat za rastlinjake, substrat za visoke grede, ekološki substrat, prodaja substrata Slovenija, kokos substrat"
+    title: "Kokovit",
+    description: "Visokokvaliteten naravni substrat iz mešanice kokosovih vlaken, šote in drugih organskih snovi, za visoke grede, vrtove, rastlinjake, okrasne grede, ločnice itd.",
+    keywords: "Kokovit, naravni substrat, kokovit, kokosova vlakna, kokos, vlakna, visoke grede, grede, vrt, rastlinjaki, rastlinjak, vrtičkanje, substrat za vrt, organski substrat, substrat za rastlinjake, substrat za visoke grede, ekološki substrat, prodaja substrata Slovenija, kokos substrat, gregor fistravec"
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Analytics />
-        <SpeedInsights />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+                <Analytics />
+                <SpeedInsights />
+            </body>
+        </html>
+    );
 }
